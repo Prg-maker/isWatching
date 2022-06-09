1f408e154a2fb6e21c462e7d15a5f5686683178b
Prg-maker <df3296709@gmail.com>
2022-06-09 20:01:25 -0300

    create visible

--- backend/src/repository/prisma/prisma-visualization-repository.ts
+++ backend/src/repository/prisma/prisma-visualization-repository.ts
@@ -1,13 +1,26 @@
 import {VisualizationData, VisualizationRepository} from '../visualization-repository'
 import {prisma} from '../../prisma'
 
+interface Return {
+  id:string;
+  visualization: number
+}
+
 
 export class PrismaVisualizationRepository implements VisualizationRepository{
   async create(){
-    
-    const visualization = await prisma.visualization.findFirst()
 
-    
+    const {visualization} = await prisma.visualization.findFirst() as Return
 
+    const valor = visualization + 1
+
+    await prisma.visualization.update({
+      where:{
+        id:"00ec84bf-cdc6-4c14-904f-48b71aec7b29"
+      },
+      data:{
+        visualization: valor
+      }
+    })
   }
 }
